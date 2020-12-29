f32aff17b06aa0eaa01f202a1ffdaa18b31de451
Polaasia <51904848+Polaasia@users.noreply.github.com>
2020-12-29 13:48:51 +0100

    Lab_03

---
+++ Lab-03/DAL/IDbService.cs
@@ -0,0 +1,13 @@
+﻿using Lab_03.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_03.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
